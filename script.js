973a69e4ae92c623f5d370188deba4ca5889dc13
andreadldev <andrea.delisi2@gmail.com>
2022-10-19 19:35:21 +0200

    Rimuovi spazi tra le lettere

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const name = prompt("Qual è il tuo nome?");
 const lastName = prompt("Qual è il tuo cognome?");
 const favColor = prompt("Qual è il tuo colore preferito?");
 
-const newPwd = `La tua nuova password è ${name}${lastName}${favColor}76`
+const newPwd = `La tua nuova password è ${name.replaceAll(' ','')}${lastName.replaceAll(' ','')}${favColor.replaceAll(' ','')}76`
 document.getElementById('pwd').innerHTML = newPwd;
